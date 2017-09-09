54db26a2427abfd75c070a7836ba1349cad5dcd0
Andy <andyaport@gmail.com>
2017-09-09 02:52:40 -0400

    Improve aesthetic of docstring.

--- pdfsplit.py
+++ pdfsplit.py
@@ -1,31 +1,34 @@
-#! /usr/bin/env python
+#!/usr/bin/python
 
 """Extract a list of pages from a pdf file into a new single pdf file.  
 
 Prerequisites:
-	`pyPdf` (available through pip).
+==============
+* `pyPdf` (available through pip).
 
 Usage:
-	python pdfsplit.py <input_file> <output_file> <pages>
+======
+`python pdfsplit.py <input_file> <output_file> <pages>`
 
-	For example, to create a new pdf named out.pdf from pages 1, 3, 5, 
-	and 7, use::
+For example, to create a new pdf named out.pdf from pages 1, 3, 5, 
+and 7, use::
 
-		$ python pdfsplit.py in.pdf out.pdf 1 3 5 7
+	$ python pdfsplit.py in.pdf out.pdf 1 3 5 7
 
-	Or, if you've followed the "How to..." below::
+Or, if you've followed the "How to..." below::
 
-		$ pdfsplit in.pdf out.pdf 1 3 5 7
+	$ pdfsplit in.pdf out.pdf 1 3 5 7
 
 How to make this script executable from anywhere (on mac or linux):
-	1. Make sure the first line of this script is #!/usr/bin/python
-	2. Make script executable::
+===================================================================
+1. Make sure the first line of this script is #!/usr/bin/python
+2. Make script executable::
 
-	    $ chmod 775 vocab.py
+    $ chmod 775 vocab.py
 
-	3. Make a link so that the terminal will find this script::
+3. Make a link so that the terminal will find this script::
 
-	    $ sudo ln -s "$PWD"/vocab.py /usr/bin/vocab
+    $ sudo ln -s "$PWD"/vocab.py /usr/bin/vocab
 
 """
 
